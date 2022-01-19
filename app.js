70f783e4d7e653bd94124f8affff68e19cd4749c
Steven Le <StevenLe928@yahoo.com>
2022-01-18 19:07:39 -0500

    added md5 package

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
 
 const app = express();
 const PORT = 3000;
@@ -19,7 +19,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = mongoose.model("User", userSchema);
 
@@ -38,7 +37,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const user = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   user.save(function(err){
     if(err){
