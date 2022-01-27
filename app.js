335c81929aa4b82c15957be5e58776045d09f43b
Steven Le <StevenLe928@yahoo.com>
2022-01-27 00:05:13 -0500

    updated callback for oauth2

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "https://sleepy-cliffs-85596.herokuapp.com/auth/google/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     // console.log(profile);
