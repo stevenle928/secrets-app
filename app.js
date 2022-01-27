1104de76995fa94ea8b1e68ca1e38c48ed0c4a38
Steven Le <StevenLe928@yahoo.com>
2022-01-27 14:11:55 -0500

    adjusted findOrCreate function to store user emails via google auth as well

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ passport.use(new GoogleStrategy({
   },
   function(accessToken, refreshToken, profile, cb) {
     // console.log(profile);
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    User.findOrCreate({ googleId: profile.id, username: profile.emails[0].value }, function (err, user) {
       return cb(err, user);
     });
   }
